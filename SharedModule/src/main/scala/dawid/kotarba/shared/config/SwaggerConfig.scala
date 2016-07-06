4b0c1b06ddd56abba5226dddf01994b0dbeb86ce
dawidkotarba <dawidkotarba@gmail.com>
2016-07-06 20:36:25 +0200

    update config

--- SharedModule/src/main/scala/dawid/kotarba/shared/config/SwaggerConfig.scala
+++ SharedModule/src/main/scala/dawid/kotarba/shared/config/SwaggerConfig.scala
@@ -11,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
   */
 @Configuration
 @EnableSwagger2
-@Profile(Array("default"))
+@Profile(Array("dev", "prod"))
 class SwaggerConfig {
 
   def api(): Docket = {
--- UsersModule/src/main/scala/dawid/kotarba/users/config/MainConfig.scala
+++ UsersModule/src/main/scala/dawid/kotarba/users/config/MainConfig.scala
@@ -1,12 +1,10 @@
 package dawid.kotarba.users.config
 
 import dawid.kotarba.shared.annotation.SpringBootConfiguration
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 /**
   * Created by Dawid on 01.07.2016.
   */
 
 @SpringBootConfiguration
-@EnableEurekaClient
 class MainConfig
