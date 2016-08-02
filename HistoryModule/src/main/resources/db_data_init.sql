f484fad8265314f53d3064e70bfbbdf6f679cfa7
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-08-02 20:43:12 +0100

    update security configs

---
+++ HistoryModule/src/main/resources/db_data_init.sql
@@ -0,0 +1,4 @@
+-- USER TABLE
+INSERT INTO public.event_types (type) VALUES
+  ('LOG IN'),
+  ('LOG OUT');
--- HistoryModule/src/main/scala/dawid/kotarba/history/config/SecurityConfig.scala
+++ HistoryModule/src/main/scala/dawid/kotarba/history/config/SecurityConfig.scala
@@ -1,11 +1,8 @@
-package dawid.kotarba.proxy.config
+package dawid.kotarba.history.config
 
 import dawid.kotarba.shared.config.extendable.PermitAllSecurityConfig
 import org.springframework.context.annotation.Configuration
 
-/**
-  * Created by Dawid on 03.07.2016.
-  */
 
 @Configuration
 class SecurityConfig extends PermitAllSecurityConfig
