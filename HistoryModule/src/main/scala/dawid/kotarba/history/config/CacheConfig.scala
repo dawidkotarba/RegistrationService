6186bacac3f1c311d5ffd7d1bd7f48b99603e23d
dawidkotarba <dawidkotarba@gmail.com>
2016-08-12 23:42:18 +0200

    add caching configuration

---
+++ HistoryModule/src/main/scala/dawid/kotarba/history/config/CacheConfig.scala
@@ -0,0 +1,10 @@
+package dawid.kotarba.history.config
+
+import dawid.kotarba.shared.config.extendable.CommonCacheConfig
+import org.springframework.context.annotation.Configuration
+
+/**
+  * Created by Dawid on 12.08.2016.
+  */
+@Configuration
+class CacheConfig extends CommonCacheConfig
--- HistoryModule/src/main/scala/dawid/kotarba/history/repository/EventTypeRepository.scala
+++ HistoryModule/src/main/scala/dawid/kotarba/history/repository/EventTypeRepository.scala
@@ -1,11 +1,15 @@
 package dawid.kotarba.history.repository
 
+
 import dawid.kotarba.history.model.entity.EventType
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.jpa.repository.JpaRepository
 
 /**
   * Created by Dawid on 03.08.2016.
   */
 trait EventTypeRepository extends JpaRepository[EventType, java.lang.Long] {
+
+  @Cacheable(cacheNames = Array("eventTypeCache"))
   def findByEventType(eventType: String): java.util.List[EventType]
 }
